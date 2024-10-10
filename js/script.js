9135c759bab16eaf766652d6ec469ef960095a01
MachinDifforme <166718742+MachinDifforme@users.noreply.github.com>
2024-10-10 22:54:34 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let currentPlayer =1;
+let currentPlayer = 1;
 
 function generateTable() {
     // creates a <table> element and a <tbody> element
@@ -7,11 +7,11 @@ function generateTable() {
     const tblBody = document.createElement("tbody");
 
     // creating all cells
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < 6; i++) {
         // creates a table row
         const row = document.createElement("tr");
 
-        for (let j = 0; j < 2; j++) {
+        for (let j = 0; j < 7; j++) {
             // Create a <td> element and a text node, make the text
             // node the contents of the <td>, and put the <td> at
             // the end of the table row
@@ -21,11 +21,9 @@ function generateTable() {
             cell.setAttribute("onclick", 'changeCellColor(this.id)');
 
         }
-
         // add the row to the end of the table body
-        tblBody.appendChild(row);
+        tblBody.prepend(row);
     }
-
     // put the <tbody> in the <table>
     tbl.appendChild(tblBody);
     // appends <table> into <body>
@@ -33,25 +31,39 @@ function generateTable() {
     // sets the border attribute of tbl to '2'
     tbl.setAttribute("id", "position");
 }
-
+function freeCell(cellId) {
+    let idCol = Array.from(cellId)[1]
+    for (let i = 0; i < 7; i++) {
+        let curCell = document.getElementById(i.toString() + idCol)
+        j = i + 1
+        let maxCell = document.getElementById('5' + idCol)
+        console.log(i.toString() + idCol)
+        if (j < 7) {
+            if (maxCell.className == 'Joueur1' || maxCell.className == 'Joueur2') {
+                alert('Column already full')
+                break
+            }
+            else if (curCell.className.length === 0) {
+                curCell.className = `Joueur${currentPlayer}`;
+                if (currentPlayer == 1) {
+                    currentPlayer = 2
+                }
+                else {
+                    currentPlayer = 1
+                }
+                break
+            }
+        }
+    }
+}
+function checkVictory(cell){
+    
+}
 function changeCellColor(cell) {
     temp = document.getElementById(cell);
-    if(!temp.className){
-        temp.className=`Joueur${currentPlayer}`;
-        if(currentPlayer == 1){
-            currentPlayer = 2
-        }
-        else{
-            currentPlayer = 1
-        }
-        console.log(temp.className)
-    }
-    else{
-        console.log("cell not empty")
-    }
+    freeCell(cell)
+    checkVictory(cell)
 }
-function displayCell(cell){
-    console.log(cell)
-}
+
 const button = document.getElementById("myButton");
 button.addEventListener("click", generateTable);
